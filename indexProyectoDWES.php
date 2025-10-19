614776279ebb84976a49bf4764020cfc2f5f2eb4
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2025-10-19 20:16:07 +0200

    20251019 - Commit inicial

---
+++ indexProyectoDWES.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Jesús Temprano Gallego</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #f4f6f9;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background: #4e9645;
+            color: white;
+            padding: 15px;
+            text-align: center;
+        }
+        h1 {
+            margin: 0;
+        }
+        main {
+            max-width: 800px;
+            margin: 30px auto;
+            padding: 20px;
+            background: white;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+        li {
+            background: #ecf0f1;
+            margin: 10px 0;
+            padding: 15px;
+            border-left: 5px solid #34db34;
+            transition: 0.3s;
+            border-radius: 8px;
+        }
+        li:hover {
+            background: #d6f8d6;
+            border-left: 5px solid #70bc1a;
+            transform: scale(1.03);
+        }
+
+        footer {
+            margin: auto;
+            background-color: #459650;
+            text-align: center;
+            height: 150px;
+            color: white;
+        }
+        main {
+            text-align: center;
+            justify-content: center;
+        }
+        a {
+            text-decoration: none;
+            color: inherit;
+        }
+        footer {
+            padding-top: 25px;
+            
+            a {
+                text-decoration: aquamarine wavy underline;
+                color: aquamarine;
+                transition: 0.3s;
+            }
+            a:hover {
+                color: blue;
+                mix-blend-mode: multiply;
+                text-decoration: none;
+            }
+        }
+    </style>
+</head>
+<body>
+    <!-- 😼 -->
+    <header>
+        <h1>CFGS - Desarrollo de Aplicaciones Web</h1>
+        <h3>Jesús Temprano Gallego</h3>
+        <p>Curso 2025/2026 - Grupo DAW2</p>
+    </header>
+    <!-- 😼 -->
+    <main>
+        <h2>ASIGNATURAS</h2>
+        <ul>
+            <a href="./doc/EstudioTema1.pdf" target="_blank"><li>UT1: Estudio teórico sobre el desarrollo de aplicaciones web del lado servidor</li></a>
+            <a href="./doc/EstudioTema2.html" target="_blank"><li>UT2: Construcción, documentación y prueba de los entornos de desarrollo y explotación para el desarrollo de aplicaciones web del lado servidor</li></a>
+            <a href="../JTGDWESProyectoTema3/indexProyectoTema3.php" target="_self"><li>UT3: Diseño y codificación de una página web dinámica sin persistencia utilizando PHP</li></a>
+            <a href="#" target="_self"><li>UT4: Diseño y codificación de una página web dinámica con persistencia en MySQL</li></a>
+            <a href="#" target="_self"><li>UT5: Control de acceso, identificación de usuarios, cookies, sesiones integrados en aplicaciones web dinámicas</li></a>
+            <a href="#" target="_self"><li>UT6: Programación Orientada a Objetos, Modelo Vista Controlador, Aplicaciones multicapa</li></a>
+            <a href="#" target="_self"><li>UT7: Uso y desarrollo de servicios web</li></a>
+            <a href="#" target="_self"><li>UT8: Desarrollo de aplicaciones web híbridas</li></a>
+        </ul>
+    </main>
+    <!-- 😼 -->
+    <footer><a href="../../" target="_self">Jesús Temprano Gallego</a> | 30/09/2025</footer>
+    <!-- 😼 -->
+    <!-- muxixima glasia alvelto pol el marivilliosiximo achetemeele que te paxo chatgepete -->
+</body>
+</html>
